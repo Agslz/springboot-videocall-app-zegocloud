8783f820dbdaac32c93d8736a6f87cd03204737a
Agustín Schulze <agsp3rs3o@gmail.com>
2024-01-23 13:45:08 -0300

    Update UserController.java

--- src/main/java/com/ags/videocall/user/controllers/UserController.java
+++ src/main/java/com/ags/videocall/user/controllers/UserController.java
@@ -4,10 +4,13 @@ import com.ags.videocall.user.User;
 import com.ags.videocall.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.IntStream;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -36,6 +39,14 @@ public class UserController {
     @GetMapping
     public List<User> findAll() {
         return userService.findAll();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception ex) {
+        ex.printStackTrace();
+        return ResponseEntity
+                .status(INTERNAL_SERVER_ERROR)
+                .body(ex.getMessage());
     }
 
 
